df823491b8ddfb317dd1c5855ac3247e1634c4aa
Shinmachi Kazuki <swc1250@gmail.com>
2018-10-18 19:25:36 +0900

    プラグインファイルの作成

---
+++ wp-sample-plugin.php
@@ -0,0 +1,10 @@
+<?php
+/*
+Plugin Name: WordPress Sample Plugin
+Plugin URI: https://github.com/ShinmachiKazuki/wp-sample-plugin
+Description: WordPress sample build.
+Version: (プラグインのバージョン番号。例: 1.0)
+Author: Kazuki Shinmachi
+Author URI: https://github.com/ShinmachiKazuki
+License: GPLv2 or later
+*/
